f4a7463658e3a554fc63632f5399b04d89958bc5
张金虎 <1151916951@qq.com>
2019-06-30 22:18:26 +0800

    问题解决~

--- paper-base/src/test/java/com/henu/paperbase/LogApplicationTests.java
+++ paper-base/src/test/java/com/henu/paperbase/LogApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ClouddoLogApplicationTests {
+public class LogApplicationTests {
 
 	@Test
 	public void contextLoads() {
